fc2598fcff34a9aa5a1a1b21f345e52511dc3a28
ahmed sayed <66169978+ahmed1958@users.noreply.github.com>
2024-09-17 23:07:07 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,8 @@
-resource "local_file" "vpc-output" {
-    content  = aws_instance.Public_EC2["hamada public"].associate_public_ip_address
-    filename = "public-output.txt"
+resource "local_file" "inventory" {
+  content = <<EOF
+[app]
+${join("\n", [for instance in aws_instance.Public_EC2 : instance.public_ip if instance.public_ip != null])}
+EOF
+  filename = "inventory.txt"
 }
+
